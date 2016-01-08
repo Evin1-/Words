8b75e29a84cd5296bd2c2977306ac8372e816bd3
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-07 22:04:04 -0500

    Add Parse compatibility

--- app/src/main/java/mx/evin/apps/words/MainActivity.java
+++ app/src/main/java/mx/evin/apps/words/MainActivity.java
@@ -2,12 +2,44 @@ package mx.evin.apps.words;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG_ = "MainActivityTAG_";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this);
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+        ParseUser user = new ParseUser();
+        user.setUsername("my_name");
+        user.setPassword("my_pass");
+        user.setEmail("email@example.com");
+
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d(TAG_, "User logged in");
+                } else {
+                    Log.d(TAG_, "Error: User NOT logged in");
+                    Log.d(TAG_, e.toString());
+                }
+            }
+        });
     }
 }
