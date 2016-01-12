e15bb88707b51db5368d7e224c4ec131630b31bb
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-12 13:31:43 -0500

    Remove unused spaces in framgents

--- app/src/main/java/mx/evin/apps/words/view/fragments/MainFragment.java
+++ app/src/main/java/mx/evin/apps/words/view/fragments/MainFragment.java
@@ -19,11 +19,9 @@ public class MainFragment extends Fragment {
 
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
-
 }
--- app/src/main/java/mx/evin/apps/words/view/fragments/StartingFragment.java
+++ app/src/main/java/mx/evin/apps/words/view/fragments/StartingFragment.java
@@ -19,7 +19,6 @@ public class StartingFragment extends Fragment {
 
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
