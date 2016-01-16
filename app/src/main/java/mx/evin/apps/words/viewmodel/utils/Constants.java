96a6c6c4371b2e974ed2f990323d3281f5b4e2ba
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-15 23:06:36 -0500

    Add type fragment enum in Constants

--- app/src/main/java/mx/evin/apps/words/viewmodel/utils/Constants.java
+++ app/src/main/java/mx/evin/apps/words/viewmodel/utils/Constants.java
@@ -6,5 +6,11 @@ package mx.evin.apps.words.viewmodel.utils;
 public class Constants {
     public static final String PREFERENCE_FILE_KEY = "mx.evin.apps.words";
     public static final String LAST_TERM_KEY = "last_term";
-    public static final String FRAGMENT_TAG_TYPE = "fragment_addTyped";
+    public static final String FRAGMENT_TAG_TYPE = "fragment_add";
+
+    public enum TYPE_ADD {
+        WRITTEN,
+        SPOKEN,
+        GOOGLED
+    }
 }
