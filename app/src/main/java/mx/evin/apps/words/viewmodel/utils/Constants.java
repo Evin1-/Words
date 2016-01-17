3d41520d86dca67b7824c2d60a0a7d14d89ff2c5
Evin1- <edwin.hdz.12.0@gmail.com>
2016-01-16 20:24:37 -0500

    Change meta-data name

--- app/src/main/java/mx/evin/apps/words/viewmodel/utils/Constants.java
+++ app/src/main/java/mx/evin/apps/words/viewmodel/utils/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
     public static final String PREFERENCE_FILE_KEY = "mx.evin.apps.words";
     public static final String LAST_TERM_KEY = "last_term";
     public static final String FRAGMENT_TAG = "fragment_add";
+    public static final String GOOGLE_API_KEY_TAG = "GOOGLE_API_KEY";
+    public static final String GOOGLE_CUSTOM_SEARCH_KEY_TAG = "GOOGLE_CUSTOM_SEARCH";
 
     public enum TYPE_ADD {
         WRITTEN,
